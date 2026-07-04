8d4e19dfae471ee7c540bd3fd2944a2e08f70530
Mallasainadh <mallasainadh469@gmail.com>
2026-07-04 16:06:06 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,34 @@
+#print(2+3)
+'''a=5
+b=8
+print(a+b)'''
+
+#run_time input()
+'''
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+print(a+b)
+'''
+'''
+a=input("datal")
+b=input("data2")
+print(a+b)'''
+
+'''a=complex(input("datal"))
+b=complex(input("data2"))
+print(a+b)'''
+
+'''a=bool(input("data1"))
+b=bool(input("data2"))
+c=bool(input("data3"))'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b) '''
+
+'''a=int(input())
+b=int(input())
+print(a+b)
