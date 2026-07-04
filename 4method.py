3d3bb1270faee6808cc094029b86f1a3c253376a
Mallasainadh <mallasainadh469@gmail.com>
2026-07-04 16:07:27 +0530

    Add files via upload

---
+++ 4method.py
@@ -0,0 +1,47 @@
+'''a=10
+b=20
+print(a)
+print(b)'''
+
+'''a=int(input())
+b=int(input())
+a,b=b,a
+print(a)
+print(b)'''
+
+'''a=10
+b=20
+
+c=a
+a=b
+b=c
+print(a)
+print(b)'''
+
+#swapping two variables using temp variable
+
+'''a=10
+b=20
+temp=a
+a=b
+b=temp
+print("a value is",a)
+print("b value is",b)'''
+
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a-%b,b=%d",a,b)'''
+
+a=float(input())
+b=float(input())
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%.2f,b=%.2f" %(a,b))
+
+
+
