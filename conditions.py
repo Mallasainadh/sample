65709b483f5eecc9fb1ca2110e55ce36b7b74689
Mallasainadh <mallasainadh469@gmail.com>
2026-07-06 15:41:15 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,156 @@
+#condition
+#if-condition by using comparision operators
+#<,>,<=,>=,!=,==
+'''a=10
+b=30
+if a>b:
+    print("true")'''
+
+'''a=10
+b=10
+if a==b:
+    print("true")'''
+
+'''a="python"
+if a=="python":
+    print("match")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b:
+    print("less")'''
+
+
+#logical operators and ,or,not
+'''a=3
+b=6
+if a<b and b>a:
+    print("true")'''
+
+'''a=4
+b=6
+if a<=b and b>=a:
+    print("true")'''
+
+'''a=9
+b=13
+if a<=b and b>=a:
+    print("true")'''
+
+'''a=2
+b=4
+if a>b or b>a:
+    print("true")'''
+
+'''a=14
+b=17
+if a<=b or b>=a:
+    print("true")'''
+
+'''a=6
+b=8
+if a!=b or a==b:
+    print("true")'''
+
+'''a=5
+b=8
+if not a<b:
+    print("true")'''
+
+'''a=5
+b=7
+if not a<b or b>a:
+    print("true")'''
+#if-conditions by using operators
+#is,is not
+
+'''a=4
+if type(a) is int:
+    print("is is int")'''
+
+'''a=5
+if type(a) is not int:
+    print("is is int")'''
+
+a=7
+if type(a) is float:
+    print("is is float")
+
+a=8
+if type(a) is not str:
+    print("is is str")
+
+b=9
+if type(b) is str:
+    print("is is str")
+
+a=6
+if type(a) is complex:
+    print("is is complex")
+
+a=9
+if type(a) is not complex:
+    print("is is complex")
+
+a=4
+if type(a) is bool:
+    print("is is bool")
+
+#if-condition by using membership operators
+'''a=2,3,4,5,6,7
+if 8 in a:
+    print("true")'''
+
+'''a=2,3,4,5,6,7,8,9
+if 20 not in a:
+    print("true")'''
+
+'''a=2,3,4,5,6,7,8,9
+b=int(input("value"))
+if b in a:
+      print("true")'''
+
+
+#if-else conditions by using compatision operators
+'''a=5
+b=7
+if a<b:
+    print("less")
+else:
+    print("false")'''
+
+'''a=5
+b=9
+if a>b:
+    print("less")
+else:
+    print("false")'''
+
+'''a=7
+b=8
+if a>b:
+    print("less")
+else:
+    print("false")'''
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
