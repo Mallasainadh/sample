c931241d9e4c85b1b2b4e8a86aec57b5110a70f8
Mallasainadh <mallasainadh469@gmail.com>
2026-07-07 16:47:41 +0530

    Add files via upload

---
+++ if-elief.py
@@ -0,0 +1,233 @@
+#if-elif-else
+'''a=4
+b=6
+if a<b:
+    print("less")
+elif b>a:
+        print("greater")'''
+
+
+'''a=4
+b=6
+if a>b:
+  print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=4
+b=6
+if a==b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=7
+b=8
+if a>b and b<a:
+    print("less")
+
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=6
+b=9
+if not a>b:
+    print("less")
+elif b<a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=6
+b=9
+if not a<b:
+    print("less")
+elif b<a:
+    print("greater")
+else:
+    print("false")'''
+
+
+#identify
+'''a=7
+if type(a) is int:
+    print("it is int")
+elif type(a) is not int:
+    print("false")'''
+
+'''a=8
+if type(a) is int:
+    print("it is int")
+elif type(a) is int:
+    print("true")'''
+
+'''a=34
+if type(a) is int:
+    print("it is int")
+elif type(a) is int:
+    print("false")'''
+
+'''a=5
+if type(a) is int:
+    print("it is int")
+elif type(a) is int:    print("false")'''
+
+#multiple-if conditions
+'''a=40
+b=60
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+
+'''a=30
+b=40
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+
+
+'''a=20
+b=40
+if a==b:
+    print("less")
+if b>a:
+    print("greater")
+if a<=b:
+    print("not equal")
+else:
+    print("true")'''
+
+'''a=50
+b=70
+if a>=b:
+    print("less")
+if b>a:
+    print("greater")
+if a>=b:
+    print("not equal")
+else:
+    print("false")'''
+
+#nested-if conditions
+'''a=5
+b=7
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a=4
+b=12
+if a!=b:
+    print("true")
+    if b==a:
+        print("greater")'''
+
+'''a=8
+b=13
+if a!=b:
+    print("true")
+    if b==a:
+        print("greater")
+else:
+            print("false")'''
+
+'''a=13
+b=15
+if a==b:
+    print("true")
+    if b>a:
+        print("greater")
+else:
+    print("false")'''
+
+
+'''a=8
+b=9
+if a!=b:
+    print("true")
+    if b==a:
+        print("greater")
+else:
+    print("not true")'''
+
+'''a=int(input())
+b=int(input())
+if a!=b:
+    print("true")
+    if b==a:
+        print("equal")
+    elif b>a:
+         print("greater")
+    else:
+        print("false")
+else:
+    print("program ends")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
