8ed456fec2de2ca6c38089dd75d09a51bf4c11ae
Mallasainadh <mallasainadh469@gmail.com>
2026-07-07 16:46:41 +0530

    Add files via upload

---
+++ if-elif-else.py
@@ -0,0 +1,27 @@
+#if-elif-else
+'''a=4
+b=6
+if a<b:
+    print("less")
+elif b>a:
+        print("greater")'''
+
+
+'''a=4
+b=6
+if a>b:
+  print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+
+a=4
+b=6
+if a==b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")
+
