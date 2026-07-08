c7bced0c801a17438c99a8f2e76032bbb9850d35
Mallasainadh <mallasainadh469@gmail.com>
2026-07-08 15:37:37 +0530

    Add files via upload

---
+++ voting.py
@@ -0,0 +1,96 @@
+#voting
+'''age=int(input("enter the age"))
+if age>=18:
+    print("eligible for vote")
+else:
+    print(" not eligible for vote")'''
+
+#even or odd
+'''num=int(input("enter the number"))
+if num%2==0:
+    print("it is even")
+else:
+    print("it is odd")'''
+
+#leap year
+'''year=int(input("enter the year"))
+if num%4==0:
+    print("leap year")
+else:
+    print("not leap year")'''
+    
+
+#guest code
+'''name=input("enter the name")
+if name=="sai":
+   print("welcome sai")
+else:
+    print("welcome guest")'''
+
+#multiple names
+'''names=["sai,vikram,monoj,karthik,sandy"]
+a=input("enter the name")
+if a in names:
+    print("welcome",a)
+else:
+    print("welcome guest")'''
+
+#vowels
+'''vowels=["a","y","h","b","k"]
+a=input("enter the letter").lower()
+if a in vowels:
+    print("it is vowel")
+else:
+    print("it is consonant")'''
+
+#nested_if
+'''username=input("enter your username")
+password=int(input("enter your password"))
+if username==a:
+    if password==1234:
+        print("login successful")
+    else:
+        print("incorrect password")
+else:
+    print("invalid iusername")'''
+
+#if else condition
+'''a="sai"
+b=12345
+username=input("enter the username")
+password=int(input("int enter the password"))
+if username=="sai" and password=="sai@12345":
+             print("login successful")
+else:
+    print("invalid credentials")'''
+
+#student_detail
+age=int(input("enter your age"))
+marks=int(input("enter your marks"))
+attendence=int(input("enter your attendence"))
+if age>=18:
+    print("eligible for vote")
+if marks>=80:
+    print("eligible for scholarship")
+if attendence>=70:
+    print("allow to write a exam")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             
