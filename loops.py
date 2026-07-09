223231af7d6ace366b7a5d9cd15da16f70e65670
Mallasainadh <mallasainadh469@gmail.com>
2026-07-09 15:04:18 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,72 @@
+#loops()
+#for,while,range,break,continue,pass
+#for loops()
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(a)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=[1,2,3,4,5]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=[5,6,7,8,9]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+
+'''b={"year":2026,"month":"july","date":9}
+for i in b:
+    print(i)
+print(type(b))
+print(type(i))
+for i in b.keys():
+    print(i)
+for i in b.values():
+    print(i)
+    print(type(b))
+    print(type(i))
+    
+for i in b.items():
+    print(i)
+    print(type(b))
+    print(type(i))'''
+
+'''b=[4+9j,3+2j]
+for i in b:
+    print(i)
+    print(type(b))
+    print(type(i))'''
+
+'''b=[True,False]
+for i in b:
+    print(i)
+    print(type(b))
+    print(type(i))'''
+
+
+
+'''a=["apple","banana","mango"]
+b=[]
+for i in a:
+    b.append(i.upper())
+print(b)'''
+
+'''a=[1,3,5,7,9,"code"]
+#[1,3,5,7,9,"c","o","d","e"]
+b=a.extend("code")
+print(a)'''
+          
