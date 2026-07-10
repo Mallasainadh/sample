c052f50fb5bdae678d067f9280b1acef478abc8b
Mallasainadh <mallasainadh469@gmail.com>
2026-07-10 15:38:50 +0530

    Add files via upload

---
+++ while loops.py
@@ -0,0 +1,123 @@
+#while loop()
+'''a=20
+while a>1:
+    print(a)'''
+
+
+'''a=30
+while a<1:
+    print(a)'''
+
+'''a=10
+while a>1:
+    print(a)
+    a=a-1'''
+
+'''a=70
+while a>=1:
+    print(a)
+    a=a-1'''
+
+'''a=30
+while a>=6:
+    a=a-1
+print(a)'''
+
+'''a=30
+while a<6:
+    a=a-1
+print(a)'''
+
+'''a=70
+while a>6:
+    print(a)
+    a+=1'''
+
+'''a=30
+while a>=1:
+    print(a)
+    a-=1'''
+
+'''a=50
+while a<=65:
+    print(a)
+    a+=1'''
+
+#voting
+'''while True:
+age=int(input("enter your age"))
+    if age>=18:
+            print("eligible for vote")
+    else:
+        print("not eligible for vote")'''
+
+#even_odd using while loop
+'''while 1:
+    num=int(input("enter your number"))
+    if num%2==0:
+            print("even")
+    else:
+        print("odd")'''
+
+#range()
+#start-stop-step
+'''for i in range(20):
+    print(i)'''
+
+'''for i in range(14,16):
+    print(i)'''
+
+'''for i in range(0,30,3):
+    print(i)'''
+
+'''for i in range(5,50,5):
+    print(i)'''
+
+'''for i in range(2,20,2):
+    print(i)'''
+
+#grade code
+'''while True:
+marks=int(input("enter your marks"))
+if marks in range(91,101):
+    print("grade-A")
+elif marks in range(81,91):
+    print("grade-B")
+elif marks in range(71,81):
+    print("grade-c")
+elif marks in range(50,71):
+    print("grade-d")
+else:
+    print("fail")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
