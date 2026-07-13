f91ca4fd5d01dbbddad71cc937e90ca02c74f32f
Mallasainadh <mallasainadh469@gmail.com>
2026-07-13 15:01:09 +0530

    Add files via upload

---
+++ ATM APPLICATION.py
@@ -0,0 +1,25 @@
+#ATM APPLICATION
+account=100000
+pwd=1234
+card=input("insert the card")
+if card=="c":
+    print("welcome sai")
+    password=int(input("enter the password"))
+    if password==pwd:
+        option=int(input('''choose the option
+                          1.balance enq
+                          2.withdraw'''))
+        if option==1:
+          print("acc bal is",account)
+        elif option==2:
+              money=int(input("enter the amount"))
+              print(money)
+              balance=account-money
+              print("rem bal acc is",balance)
+        else:
+             print("invalid option")
+    else:
+         print("invalid password")
+else:
+     print("invalid card")
+              
