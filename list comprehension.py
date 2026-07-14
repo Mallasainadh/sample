7038b1462ce75614c6ff3a1d4ecb90580edce87d
Mallasainadh <mallasainadh469@gmail.com>
2026-07-14 14:39:10 +0530

    Add files via upload

---
+++ list comprehension.py
@@ -0,0 +1,71 @@
+#list comprehension
+a=["CODEGNAN","PYTHON","COURSE"]
+#["CODEGNAN","PYTHON","COURSE"]
+#print(a.upper())
+
+'''b=str(a)
+print(b.upper())'''
+
+'''for i in a:
+    print(i.upper(),end=",")'''
+
+'''b=[]
+for i in a:
+    b.append(i.upper())
+    print(b)'''
+
+#syntax
+#a=[expr for var in collection/range]
+'''a=[i.upper() for i in a]
+print(a)'''
+
+'''a=["vij","hyd","vzg"]
+#["vij","hyd","vzg"]
+b=[i.title() for i in a]
+print(b)'''
+
+'''a=[1,2,3,5,6,8,12,13]
+#[1,4,9,25,36,64,144,169]
+b=[pow(i,2) for i in a]
+b=[i*i for i in a]
+b=[i**2 for i in a]
+print(b)'''
+
+#if -usage in list comprehension
+'''a=[i for i in range(16)]
+print(a)'''
+
+'''a=[i for i in range(16) if i%2==0]
+print(a)'''
+
+'''a=[i for i in range(16) if i%2!=0]
+print(a)'''
+
+
+'''a=[i**2 for i in range(21) if i%2==0]
+print(a)'''
+
+a=["apple","banana","grapes","mango","kiwi","dragon","berry"]
+for i in range(b):
+print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
