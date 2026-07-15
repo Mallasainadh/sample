0516ee535cb4a764cd37824a67dbdaf4a4ad1bf2
Mallasainadh <mallasainadh469@gmail.com>
2026-07-15 18:12:16 +0530

    Add files via upload

---
+++ functions().py
@@ -0,0 +1,146 @@
+#functions
+'''a=10
+b=20
+print("the sum is",a+b)
+print("the diff is",a-b)
+print("the product is",a*b)
+a=100
+b=200
+print("the sum is",a+b)
+print("the diff is",a-b)
+print("the product is",a*b)'''
+
+'''def calculate(a,b):
+    print("the sum is",a+b)
+    print("the diff is",a-b)
+    print("the product is",a*b)
+calculate(10,20)
+calculate(100,200)
+calculate(1000,2000)'''
+
+'''def clculate (a,b):
+print("the integer devision is",a//b)
+print("the mod  is",a%b)
+print("the power is",a**b)'''
+
+'''def add(a,b):
+    print(a+b)
+add(4,5)'''
+
+'''while True:
+    def cal():
+        a=int(input("a value"))
+        b=int(input("b value"))
+        print(a+b)
+    cal()'''
+
+'''def cal():
+    a=int(input("a value"))
+    b=int(input("b value"))
+    print(a+b)
+    cal()
+cal()'''
+
+'''def fullname():
+    fname=input("first name")
+    lname=input("last name")
+    print((fname+" "+lname).title())
+fullname()'''
+
+'''def mul(a,b):
+    print(a*b)
+mul(3,5)'''
+
+'''def mul(a,b):
+    return a*b
+print(mul(2,3))'''
+
+#print v/s return
+'''def add(a,b):
+    c=a+b
+    d=a-b
+    e=a*b
+    print(c)
+    print(d)
+    print(e)
+add(5,6)'''
+
+'''def add(a,b):
+    c=a+b
+    d=a-b
+    e=a*b
+    #return c
+    #return d
+    #return e
+    return c,d,e
+print(add(6,8))'''
+
+#splitbill()
+'''def bill():
+    bill_amount=int(input("enter the amount"))
+    persons=int(input("no of customers"))
+    total=bill_amount/persons
+    print(total)
+bill()'''
+
+'''def splitbill():
+    a=int(input("total members"))
+    b=int(input("total amount"))
+    print("perhead bill is{}".format (b//a))
+    print(f"perhead bill is {b//a}")
+splitbill()'''
+
+
+def cal (a,b):
+    a=int(input("enter the value a"))
+    b=int(input("enter the value b"))
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
