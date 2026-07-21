3dfdc9ea5b48680155cb98cda6ff539a798ecb48
Mallasainadh <mallasainadh469@gmail.com>
2026-07-21 13:59:07 +0530

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,23 @@
+#patterns
+#right angle
+
+n=int(input())
+for i in range(1,n+1):
+    print("*"*i)
+
+#reverse
+'''n=int(input())
+for i in range(n,0,-1):
+    print("*"*i)'''
+
+
+#square
+'''n=int(input())
+for i in range(n):
+    print("*"*n)'''
+
+#pyramid
+'''n=int(input())
+for i in range(1,n+1):
+    print(" " * (n-i)+ "* " * i)'''
+
