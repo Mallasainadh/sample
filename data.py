ddbb8557cf1a72abe3e09da5fad0ca27d36e33f9
Mallasainadh <mallasainadh469@gmail.com>
2026-07-25 14:44:13 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,15 @@
+'''import mymodules
+mymodules.greetings("sainadh")'''
+
+
+#import mymodules
+
+#import mymodules
+
+'''import mymodules
+b=mymodules.details["idnos"]
+c=mymodules.details["names"]
+print(b)
+print(c)'''
+    
+import mymodules
