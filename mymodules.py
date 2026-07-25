7649762e72066cc3514d3cad9d6899d84f1b61b9
Mallasainadh <mallasainadh469@gmail.com>
2026-07-25 14:45:00 +0530

    Add files via upload

---
+++ mymodules.py
@@ -0,0 +1,29 @@
+'''def greetings(name):
+    print("welcome",name)'''
+
+'''a=4
+b=8
+print(a+b)
+
+a=int(input("a value"))
+b=int(input("b value"))
+print(a+b)'''
+
+details={"idnos":[10,20,30],
+         "names":["sai","srinu","dinesh"],
+         "marks":[50,60,70]}
+
+'''if __name__=="__main__":
+    a=[10,20,30,40,50]
+    a.append("code")
+    a.extend("code")
+    print(a)'''
+
+'''def dummy():
+    if __name__=="__main__":
+        print("this program is run as script")
+    else:
+        print("this program is run as module")
+dummy()'''
+     
+ 
