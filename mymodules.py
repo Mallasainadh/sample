588a3612bfeef4d06d05e96448f8c74fc048570a
Mallasainadh <mallasainadh469@gmail.com>
2026-07-27 13:51:28 +0530

    Add files via upload

--- mymodules.py
+++ mymodules.py
@@ -25,5 +25,78 @@ details={"idnos":[10,20,30],
     else:
         print("this program is run as module")
 dummy()'''
-     
+
+#math module
+'''import math
+print(math.pi)
+print(math.pi*4)
+print(math.sqrt(2))
+print(math.log(2))
+print(math.tan(45))
+print(math.cos(60))
+print(math.sin(30))
+print(math.pow(2,4))
+print(math.ceil(6.9))
+print(math.floor(3.11))'''
+
+'''from math import pi,sqrt,log,tan
+print(pi)
+print(sqrt(4))
+print(log(6))
+print(tan(45))'''
+
+#sys module
+
+'''import sys
+print(sys.version)
+print(sys.path)'''
+
+#os module
+'''import os
+print(os.path)
+print(os.getcwd())
+print(os.listdir())
+print(os.chdir("C:\\Users\\Administrator\\Downloads"))
+print(os.listdir())'''
+
+#random module
+'''import random
+a=random.sample(range(10,40),40)
+print(a)''' #error
+
+#randint()
+'''import random
+a=random.randint(50,60)
+print(a)'''
+
+#coice()
+'''import random
+a=[10,40,50,60,70]
+b=random.choice(a)
+print(b)'''
+
+#dice code
+'''import random
+while True:
+    input("enter the roll of dice")
+    a=random.randint(1,6)
+    print(a)
+    option=input("roll agian? (y/n)")
+    if option=="y":
+        continue
+    elif option=="n":
+        break
+    else:
+        print("invaild option")'''
+
+
+
  
+
+
+
+
+
+
+
+
