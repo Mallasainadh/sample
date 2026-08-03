ed7502580c311784de37bea326ab028f39970a08
Mallasainadh <mallasainadh469@gmail.com>
2026-08-03 14:35:12 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,69 @@
+#oops
+#syntax
+#class declaration
+
+'''class classname():
+    #attritude
+    name="sai"
+    age=23
+    place="vja"
+    def fname(method_name):
+        print("statements")
+a=classname()
+a.fname()'''
+
+#class declasration
+'''class details():
+    name="sai"
+    age=23
+    place="vja"
+    def display (self):
+        print(self.name,self.age,self.place)
+a=details
+print(dir(a))
+a.display()'''
+
+#object instantiaction
+'''class details():
+    def data(self,name,age,place):
+        self.name=name
+        self.age=age
+        self.place=place
+    def display(self):
+     print(self.name,self.age,self.place)
+a=details()
+print(dir(a))
+a.data("satya",22,"vja")
+a.display()
+b=details()
+b.data("veera",24,"vja")
+b.display()'''
+
+#constructor = _init_ or new = we can give the values directly in class
+'''class Details():
+    def __init__(self,name,age,place):
+        self.name=name
+        self.age=age
+        self.place=place
+    def display(self):
+        print(self.name,self.age,self.place)
+a=Details("sai",22,"vja")
+a.display()'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
