c6f574ea96cc086b5b88633f278bc948075bb46f
Mallasainadh <mallasainadh469@gmail.com>
2026-08-05 21:05:45 +0530

    Add files via upload

--- oops.py
+++ oops.py
@@ -155,6 +155,96 @@ a.speak()
 b.speak()'''
 
 
+'''class car():
+    def vehicle(self):
+        print("Thar")
+class bike():
+    def vehicle(self):
+        print("vespa")
+a=car()
+b=bike()
+a.vehicle()
+b.vehicle()'''
+
+#single inheritance
+'''class RBI():#parent class
+    cash=100000
+    def available_cash(cls):
+        #print("available_cash is",cls.cash)
+        print("available_cash is",RBI.cash)
+class SBI(RBI):#child-1
+    pass
+class HDFC(RBI):#child-2
+    cash=50000
+    def new_cash(cls):
+        #print("new cash is",cls.cash+cls.cash)
+        print("new cash is",cls.cash+RBI.cash)
+a=HDFC()
+a.available_cash()
+a.new_cash()'''
+
+#multiple-inheritance
+'''class father():
+    def height(self):
+       print("height is 5.5 inches")
+class mother():
+    def weight(self):
+        print("weight is 60kgs")
+class kid():
+    def dob(self):
+        print("just born....")
+a=father()
+b=mother()
+c=kid()
+a.height()
+b.weight()
+c.dob()'''
+
+#multi-level-inheritance
+'''class grandparent():
+    def land(self):
+        print("10 acres")
+class parent(grandparent):
+    def house(self):
+        print("100 sqft")
+class child(parent):
+    def car(self):
+        print("BMW")
+a=child()
+a.land()
+a.house()
+a.car()'''
+
+#hierarchical-inheritance
+class Employee():#parent
+    def company(self):
+        print("codegnan it soluction")
+class Trainer(Employee):#child1
+    def teaching(self):
+        print("trainer teach the code")
+class Developer(Employee):#child2
+    def code(self):
+        print("developer develops the code")
+a=Trainer()
+a.teaching()
+a.company()
+b=Developer()
+b.code()
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
